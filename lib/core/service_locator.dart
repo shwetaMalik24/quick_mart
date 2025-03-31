673084e6d70110f75a88689f1ece3519430a513f
shweta malik <malikshweta24@gmail.com>
2025-03-31 23:42:19 +0530

    Used usecase in cubit and register the same.

--- lib/core/service_locator.dart
+++ lib/core/service_locator.dart
@@ -1,6 +1,7 @@
 import 'package:get_it/get_it.dart';
 import 'package:quickmart/data/data_source/user_remote_data_source.dart';
 import 'package:quickmart/data/repository/user_repository_impl.dart.dart';
+import 'package:quickmart/domain/use_case/user_use_case.dart';
 import 'package:quickmart/presentation/user/user_login/bloc/login_cubit.dart';
 
 final GetIt getIt = GetIt.instance;
@@ -14,9 +15,15 @@ void setupLocator() {
       getIt<UserRemoteDataSource>(),
     ),
   );
+
+  getIt.registerLazySingleton<UserLoginUseCase>(
+    () => UserLoginUseCase(
+      getIt<UserRepositoryImpl>(),
+    ),
+  );
   getIt.registerFactory<LoginCubit>(
     () => LoginCubit(
-      getIt<UserRepositoryImpl>(),
+      getIt<UserLoginUseCase>(),
     ),
   );
 }
--- lib/presentation/user/user_login/bloc/login_cubit.dart
+++ lib/presentation/user/user_login/bloc/login_cubit.dart
@@ -1,19 +1,20 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:quickmart/domain/repository/user_repository.dart';
 import 'package:quickmart/domain/entity/user_entity.dart';
+import 'package:quickmart/domain/use_case/user_use_case.dart';
 
 part 'login_cubit.freezed.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  final UserRepository _userRepository;
+  final UserLoginUseCase _loginUseCase;
 
-  LoginCubit(this._userRepository) : super(const LoginState.initial());
+  LoginCubit(this._loginUseCase) : super(const LoginState.initial());
 
   Future<void> login(String email, String password) async {
     try {
       emit(const LoginState.loading());
-      final user = await _userRepository.login(email, password);
+      final user =
+          await _loginUseCase.execute(email, password); // ✅ Calls Use Case
       emit(LoginState.success(user));
     } catch (e) {
       emit(LoginState.failure(e.toString()));
